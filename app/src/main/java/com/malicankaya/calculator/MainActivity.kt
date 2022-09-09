4c6e6e6248cfb26ad1945c94df3daab0f09f5ab0
malicankaya <malicankaya97@gmail.com>
2022-09-09 19:28:44 +0300

    add functionality to CLR and onClick imlement

--- app/src/main/java/com/malicankaya/calculator/MainActivity.kt
+++ app/src/main/java/com/malicankaya/calculator/MainActivity.kt
@@ -2,10 +2,28 @@ package com.malicankaya.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view: View) {
+        val btn = view as Button
+        tvInput?.append(btn.text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
     }
 }
